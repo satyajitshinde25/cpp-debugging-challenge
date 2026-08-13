acc54f0194033270623d958944a0a68b8c113e03
satyajitshinde25 <satyajit.shinde25@pccoepune.org>
2026-08-13 12:11:07 +0530

    fix: correct gcd recursive implementation

--- questions/question-03-maintenance-schedule.cpp
+++ questions/question-03-maintenance-schedule.cpp
@@ -9,9 +9,9 @@ using namespace std;
 int gcd(int a, int b)
 {
     if (b == 0)
-        return b;
+        return a;
 
-    return gcd(a % b, b);
+    return gcd(b,a % b);
 }
 
 int lcm(int a, int b)
